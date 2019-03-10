0c22c51e5199622a3c5d3984fd9f3fe001df0db6
kestory <kestory@163.com>
2019-03-10 11:25:20 +0800

    2019.3.10

--- 15-二进制中1的个数.cpp
+++ 15-二进制中1的个数.cpp
@@ -54,3 +54,21 @@ int main( )
 
 	return 0;
 }
+
+
+java源码中Integer.bitCount的实现
+https://segmentfault.com/a/1190000015763941
+class Solution
+{
+public:
+	int NumberOf1(int i)
+	{
+		i = i - ((i >> 1) & 0x55555555);
+		i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+		i = (i + (i >> 4)) & 0x0f0f0f0f;
+		i = i + (i >> 8);
+		i = i + (i >> 16);
+		return i & 0x3f;
+	}
+
+};
